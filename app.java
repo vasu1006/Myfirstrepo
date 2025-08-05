e35c8d42aba0a7b8a88032f33fd061d349e254fc
vasu1006 <vasusabbavarapu1612@gmail.com>
2025-08-05 06:18:14 +0530

    Update app.java

--- app.java
+++ app.java
@@ -34,3 +34,15 @@ public class QuotientRemainder {
         System.out.println("Second number = " + second);
     }
 } 
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
